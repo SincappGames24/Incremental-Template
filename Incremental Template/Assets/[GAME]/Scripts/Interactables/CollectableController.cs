2089b3c707ec224907393eb6b6d5e00e6a014b5a
Faruk Hekimoğlu <faruk.hekimoglu@hotmail.com>
2023-07-07 23:31:27 +0300

    zx

---
+++ Incremental Template/Assets/[GAME]/Scripts/Interactables/CollectableController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollectableController : MonoBehaviour
+{
+    private const int _playerLayer = 8;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == _playerLayer)
+        {
+            EventManager.OnCollectable?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Incremental Template/Assets/[GAME]/Scripts/Interactables/ObstacleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    private const int _playerLayer = 8;
+    private int _counter;
+    private int _bulletCounter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == _playerLayer)
+        {
+            other.GetComponent<PlayerMovementController>().BounceBackGate();
+
+            _counter++;
+         
+            if (_counter == 3)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
