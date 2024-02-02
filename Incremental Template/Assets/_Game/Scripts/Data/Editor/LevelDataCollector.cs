47f461d07fa5d129bef3801c6f9cca99f267ef7b
Faruk Hekimoğlu <faruk.hekimoglu@hotmail.com>
2024-02-02 15:56:20 +0300

    data collect system

--- Incremental Template/Assets/_Game/Scripts/Data/Editor/LevelDataCollector.cs
+++ Incremental Template/Assets/_Game/Scripts/Data/Editor/LevelDataCollector.cs
@@ -1,8 +1,4 @@
-using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Newtonsoft.Json.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -16,26 +12,26 @@ public class LevelDataCollector : EditorWindow
         GetWindow<LevelDataCollector>("Prefab to ScriptableObject");
     }
 
-    private List<IInteractable> selectedInteractables = new List<IInteractable>();
-
     private void OnGUI()
     {
         GUILayout.Label("Select interactables and press the button to create Scriptable Objects for their data.");
 
         _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
         DisplayInteractableSelection();
-
+        SaveDatas();
         EditorGUILayout.EndScrollView();
     }
 
     private void DisplayInteractableSelection()
     {
         EditorGUILayout.BeginVertical();
+        
+        bool anyObjectSelected = Selection.gameObjects.Length > 0;
+
+        GUI.enabled = anyObjectSelected;
 
         foreach (GameObject obj in Selection.gameObjects)
         {
-            string prefabName = obj.name;
-
             IInteractable[] interactableList = obj.GetComponentsInChildren<IInteractable>();
 
             foreach (IInteractable interactableObject in interactableList)
@@ -43,52 +39,56 @@ public class LevelDataCollector : EditorWindow
                 if (interactableObject != null)
                 {
                     EditorGUILayout.ObjectField(interactableObject.GetType().ToString(), obj, typeof(GameObject), true);
-
-                    if (!selectedInteractables.Contains(interactableObject))
-                    {
-                        selectedInteractables.Add(interactableObject);
-                    }
                 }
             }
+        }
+    }
 
-            if (GUILayout.Button("Save Interactable Variables as ScriptableObject") && selectedInteractables.Count > 0)
+    private void SaveDatas()
+    {
+        if (GUILayout.Button("Save Interactable Variables as ScriptableObjects"))
+        {
+            for (int i = 0; i < Selection.gameObjects.Length; i++)
             {
-                CreateScriptableObjectsForInteractables(selectedInteractables, prefabName);
+                InteractableDataContainer dataContainer = ScriptableObject.CreateInstance<InteractableDataContainer>();
+
+                GameObject obj = Selection.gameObjects[i];
+                string prefabName = obj.name;
+                string path = "Assets/_Game/Prefabs/Levels/Resources/LevelDatas/" + prefabName + "_Data.asset";
+
+                IInteractable[] interactableList = obj.GetComponentsInChildren<IInteractable>();
+
+                foreach (IInteractable interactableObject in interactableList)
+                {
+                    if (interactableObject != null)
+                    {
+                        string prefabPath =
+                            PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(interactableObject
+                                .GetGameObjectReference());
+                        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+
+                        if (prefab != null)
+                        {
+                            InteractableData data = interactableObject.GetInteractableData();
+                            data.InteractableReference = prefab;
+                            dataContainer.interactableDataList.Add(data);
+                        }
+                        else
+                        {
+                            Debug.LogError("Object is not a Prefab! " + interactableObject.ToString());
+                        }
+                    }
+                }
+
+                AssetDatabase.CreateAsset(dataContainer, path);
             }
 
-            selectedInteractables.Clear();
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            Debug.Log("Scriptable Objects for interactables created for selected prefabs.");
         }
 
         EditorGUILayout.EndVertical();
     }
-
-    private void CreateScriptableObjectsForInteractables(List<IInteractable> selectedInteractables,string prefabName)
-    {
-        InteractableDataContainer dataContainer = ScriptableObject.CreateInstance<InteractableDataContainer>();
-
-        foreach (var interactable in selectedInteractables)
-        {
-            string prefabPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(interactable.GetGameObjectReference());
-            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
-
-            if (prefab != null)
-            {
-                InteractableData data = interactable.GetInteractableData();
-                data.InteractableReference = prefab;
-                dataContainer.interactableDataList.Add(data);
-            }
-            else
-            {
-                Debug.LogError("Obje Prefab Degil! " + interactable.ToString());
-            }
-        }
-
-        string path = "Assets/_Game/Prefabs/Levels/Resources/LevelDatas/" + prefabName + "_Data.asset";
-        AssetDatabase.CreateAsset(dataContainer, path);
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-
-        Debug.Log("Scriptable Object for interactables created!");
-    }
 }
-
